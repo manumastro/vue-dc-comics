7baa8b538d2250655a96fdb14f0c836281953583
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-05-16 16:18:24 +0200

    cards stampate tramite props

---
+++ src/components/ComicsCards.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="d-flex justify-content-center align-items-start">
+    <a class="d-flex justify-content-center align-items-center flex-column" href="#">
+      <img :src="comicElement.thumb" :alt="comicElement.series">
+      <span>{{comicElement.series}}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComicsCards',
+  props: {
+    comicElement: Object,
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/_mixins.scss';
+@import '../assets/style/_vars.scss';
+
+div{
+      width: calc(100% / 6);
+      margin: 20px 0;
+      a{
+        text-decoration: none;  
+        span{
+          align-self: flex-start;
+          text-transform: uppercase;
+          color: white;
+          width: 185px;
+        }
+        img{
+          width: 185px;
+          height: 185px;
+          object-fit: cover;
+          object-position: 100% 0;
+          margin-bottom: 15px;
+        }
+      } 
+    }
+</style>
--- src/components/ContentComponent.vue
+++ src/components/ContentComponent.vue
@@ -3,14 +3,8 @@
     <section id="jumbotron"></section>
     <section id="comics">
       <div class="container d-flex flex-wrap">
-        <div class="comic-card d-flex justify-content-center align-items-start"
-        v-for="(comic, index) in comicsData" :key="`comic-${index}`"
-        >
-          <a class="d-flex justify-content-center align-items-center flex-column" href="#">
-            <img :src="comic.thumb" :alt="comic.series">
-            <span>{{comic.series}}</span>
-          </a>
-        </div>
+        <ComicsCards v-for="(comic, index) in comicsData" :key="`comic-${index}`"
+        :comicElement="comic" />
       </div>
     </section>
   </div>
@@ -18,7 +12,9 @@
 
 <script>
 import comics from '../assets/data/dc-comics.json';
+import ComicsCards from './ComicsCards.vue';
 export default {
+  components: { ComicsCards },
   name: 'ContentComponent',
   data(){
     return{
@@ -38,26 +34,5 @@ export default {
   #comics{
     padding: 30px 0 80px 0;
     background-color: #1c1c1c;
-    .comic-card{
-      width: calc(100% / 6);
-      margin: 20px 0;
-      a{
-        text-decoration: none;  
-        span{
-          align-self: flex-start;
-          text-transform: uppercase;
-          color: white;
-          width: 185px;
-        }
-        img{
-          width: 185px;
-          height: 185px;
-          object-fit: cover;
-          object-position: 100% 0;
-          margin-bottom: 15px;
-        }
-      }
-      
-    }
   }
 </style>
